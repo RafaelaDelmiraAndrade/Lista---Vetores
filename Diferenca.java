c3e2607c760efc61c41692d56bce5093eefbba32
Rafaela delmira Andrade <rdelmira18@gmail.com>
2021-11-18 19:15:22 -0200

    Diferenca
    Programa de diferença.

---
+++ Diferenca.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Diferenca {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		final int TAM=10;
+		int i, j=0, k=0, a[], b[], c[];
+		a = new int[TAM];
+		b = new int[TAM];
+		c = new int[TAM];
+		boolean encont;
+		
+		//lendo o vetor A
+		for(i=0; i<TAM; i++) {
+			System.out.println("Insira "+(i+1)+"° número do vetor A: ");
+			a[i] = in.nextInt();
+		}
+		
+		//lendo o vetor B
+		System.out.println();
+		for(i=0; i<TAM; i++) {
+			System.out.println("Insira "+(i+1)+"°um número do vetor B: ");
+			b[i] = in.nextInt();
+		}
+		
+		for(i=0; i<TAM; i++) {
+			encont = false;
+			for(j=0; j<TAM; j++) {
+				if(a[i] == b[j]) {
+					encont = true;
+				}
+			}
+			if(encont == false) {
+				c[k] = a[i];
+				k++;
+			}
+		}
+		System.out.print("\nA = [");
+		for(i=0; i<TAM; i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.print("]");
+		System.out.println();
+		System.out.print("\nB = [");
+		for(i=0; i<TAM; i++) {
+			System.out.print(b[i]+" ");
+		}
+		System.out.print("]");
+		System.out.println();
+		System.out.print("\nC = [");
+		for(i=0; i<TAM; i++) {
+			System.out.print(c[i]+" ");
+		}
+		System.out.print("]");
+	}
+
+}
